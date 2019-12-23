3c90427e438bdc8b1c793842b14c0a7ba048b63f
gabygmth <gabriela.guaman@thoughtworks.com>
2019-12-23 12:27:37 -0500

    [Jeff/Gaby] Refactoring xml beans WIP

--- src/main/java/com/quichi/blog/BlogApplication.java
+++ src/main/java/com/quichi/blog/BlogApplication.java
@@ -1,8 +1,10 @@
 package com.quichi.blog;
 
+import com.quichi.blog.config.AppConfig;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 @SpringBootApplication
 public class BlogApplication {
@@ -10,6 +12,8 @@ public class BlogApplication {
 
 
 	public static void main(String[] args) {
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.register(AppConfig.class);
 		SpringApplication sa = new SpringApplication(BlogApplication.class);
 		sa.setBannerMode(Banner.Mode.OFF);
 		sa.run(args);
--- src/main/java/com/quichi/blog/config/AppConfig.java
+++ src/main/java/com/quichi/blog/config/AppConfig.java
@@ -1,38 +1,41 @@
 package com.quichi.blog.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.quichi.blog")
 @PropertySource("classpath:application.properties")
 public class AppConfig {
-    public AppConfig() {
-    }
 
-    @Autowired
+    @Resource
     Environment environment;
 
-//    private final String URL = environment.getProperty("spring.datasource.url");
-//    private final String USER = environment.getProperty("spring.datasource.username");
-//    private final String PASSWORD = environment.getProperty("spring.datasource.password");
-//    private final String DRIVER = environment.getProperty("spring.datasource.driverClassName");
-//
-//    @Bean
-//    public JdbcTemplate getJdbcTemplate() {
-//        return new JdbcTemplate(dataSource());
-//    }
-//
-//    @Bean
-//    DataSource dataSource() {
-//        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-//        driverManagerDataSource.setUrl(environment.getProperty(URL));
-//        driverManagerDataSource.setUsername(environment.getProperty(USER));
-//        driverManagerDataSource.setPassword(environment.getProperty(PASSWORD));
-//        driverManagerDataSource.setDriverClassName(environment.getProperty(DRIVER));
-//        return driverManagerDataSource;
-//    }
+    private final String URL = environment.getProperty("spring.datasource.url");
+    private final String USER = environment.getProperty("spring.datasource.username");
+    private final String PASSWORD = environment.getProperty("spring.datasource.password");
+    private final String DRIVER = environment.getProperty("spring.datasource.driverClassName");
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    DataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setUrl(URL);
+        driverManagerDataSource.setUsername(USER);
+        driverManagerDataSource.setPassword(PASSWORD);
+        driverManagerDataSource.setDriverClassName(DRIVER);
+        return driverManagerDataSource;
+    }
 }
